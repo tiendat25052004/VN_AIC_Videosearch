1eb1a6812a11df69dcad31d2798f5d74952d9188
phonghocode <ntp11892vt@gmail.com>
2024-09-09 20:55:11 +0700

    update

--- frontend/ai/src/helper/web_url.js
+++ frontend/ai/src/helper/web_url.js
@@ -1,8 +1,8 @@
 //BACKEND
-export const web_url = "http://localhost:8080";
+export const web_url = "http://20.6.129.131:8080";
 
 //SOCKET | DB SERVER
-export const socket_url = "http://localhost:5000";
+export const socket_url = "http://20.6.129.131:5000";
 
 //COMPETITION SUBMISSION SEVER
 export const server = "";
