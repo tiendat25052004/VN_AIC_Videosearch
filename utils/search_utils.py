86c569d89553c85fa0211d2b22d6c5ce1a85bf59
phonghocode <ntp11892vt@gmail.com>
2024-09-29 02:42:44 +0700

    update

--- utils/search_utils.py
+++ utils/search_utils.py
@@ -100,11 +100,14 @@ def filter_results(results, asr_results=None, ocr_results=None):
     if asr_results is not None:
         videos = [ f"{asr['L']}_{asr['V']}" for asr in asr_results]
         scores = [asr["score"] for asr in asr_results]
+        print(videos)
+        print(scores)
         for result in results:
             if result['video_id'] in videos:
                 score = scores[videos.index(result['video_id'])]
                 for i in range(len(result["video_info"]["lst_scores"])):
                     result["video_info"]["lst_scores"][i] = result["video_info"]["lst_scores"][i] + score
+    print(results)
     for result in results:
         for path, idx, keyframe_idx, score in zip(result['lst_keyframe_paths'], result['lst_idxs'], result['lst_keyframe_idxs'], result['lst_scores']):
             result_list.append({"id": idx, "keyframe_path": path, "keyframe_id": keyframe_idx, "score": score})
